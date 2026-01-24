41996b4299635548c2a55e5d8f64c3cda27212f2
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-01-24 22:25:06 +0530

    Time: 0 ms (100%), Space: 61.5 MB (39.06%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.js
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.js
@@ -12,16 +12,15 @@
  */
 var removeElements = function (head, val) {
 
-    let sentinel = new ListNode();
-    sentinel.next = head
-
+let sentinel = new ListNode();
+    sentinel.next = head;
 
     let curr = sentinel;
-    while (curr && curr.next) {
+    while (curr&&curr.next) {
         if (curr.next.val === val) {
-            curr.next = curr.next.next
+            curr.next = curr.next.next;
         } else {
-            curr = curr.next;
+            curr = curr.next
         }
     }
 
