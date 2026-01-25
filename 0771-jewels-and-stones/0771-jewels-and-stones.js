c93a114777e2febc6a12c4a6aa66651bfb0be6fd
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-01-25 23:49:10 +0530

    Time: 2 ms (13.39%), Space: 54.3 MB (57.3%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function (jewels, stones) {
+    let count = 0;
+
+    for (let i = 0; i < stones.length; i++) {
+
+        for (let j = 0; j < jewels.length; j++) {
+            if (jewels[j] === stones[i]) {
+                ++count;
+            }
+        }
+    }
+
+    return count;
+};
