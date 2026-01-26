8d6f2af246130ea6ce44c000921c3222b6e66a53
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-01-26 21:52:53 +0530

    Time: 9 ms (22.62%), Space: 65.4 MB (5.09%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -5,14 +5,14 @@
 var isPalindrome = function (s) {
     s = s.toLowerCase();
     let filteredString = ''
-
+    let rev = ''
     for (let i = 0; i < s.length; i++) {
         if (/[a-z0-9]/.test(s[i])) {
             filteredString += s[i]
+            rev = s[i] + rev;
         }
     }
 
-    let rev = filteredString.split('').reverse().join('')
 
     return filteredString === rev
 };
