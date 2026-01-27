bdf2a3f860fb61f8352cb76fed5cf3d6028f4259
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-01-27 21:44:44 +0530

    Time: 2 ms (44.02%), Space: 54.2 MB (70.98%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let k = 0;
+
+    while (k < strs[0].length) {
+        let ch = strs[0][k]
+        for (let i = 1; i < strs.length; i++) {
+
+            if (ch != strs[i][k] || k == strs[i].length) {
+                return strs[0].substring(0, k)
+            }
+
+
+        }
+        k++
+
+    }
+
+    return strs[0]
+};
